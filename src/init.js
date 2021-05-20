cf136a0be57cf28190d7235e2c735c835b454c46
zhengyunqing <zhengyunqing@kuaishou.com>
2021-05-20 11:04:31 +0800

    feat: 生成ast树

--- src/init.js
+++ src/init.js
@@ -1,4 +1,5 @@
 import { initState } from "./state";
+import {compilerToFunction} from "./compiler/index";
 
 export function initMixin(Vue) {
     Vue.prototype._init = function(options) {
@@ -7,5 +8,27 @@ export function initMixin(Vue) {
 
         // 对数据初始化 watch 、 data 、....
         initState(vm);
+
+        // 将数据挂载到模板上
+        if(vm.$options.el) {
+            vm.$mount(vm.$options.el)
+        }
+    }
+
+    Vue.prototype.$mount = function(el) {
+        const vm = this;
+        const options = vm.$options;
+        el = document.querySelector(el);
+        // 把模板转换成对应的渲染函数  =》虚拟dom  vnode  diff算法 更新虚拟
+        // render优先级更高
+        if(!options.render) { // 无render  用template
+            let template = options.template;
+            if(!template && el) {  // 取el内容作为模板
+                template = el.outerHTML;
+                let render = compilerToFunction(template);  // 把模板变为渲染函数
+
+                options.render = render;
+            }
+        }
     }
 }
